9e8d49c098cef4dff68dcfc567e2ba6e4010769c
Siddharth Bharadwaj <siddharthbharadwaj45@gmail.com>
2020-04-19 11:43:31 +0530

    Colt: Initialize Updater

---
+++ src/com/colt/updater/model/MaintainerInfo.java
@@ -0,0 +1,14 @@
+package com.colt.updater.model;
+
+public class MaintainerInfo {
+    private String name;
+    private String github_username;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUsername() {
+        return github_username;
+    }
+}
